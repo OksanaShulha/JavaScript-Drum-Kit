2b6624cdd6aaca1280b92c38d37a3eac37249a53
OksanaShulha <shushullja@gmail.com>
2021-06-20 22:41:40 +0200

    Update html

--- src/handlers/handlers.js
+++ src/handlers/handlers.js
@@ -2,7 +2,7 @@
 const removeTransition = (e) => {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
-}
+};
 
 const playSound = (e) => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
@@ -12,6 +12,6 @@ const playSound = (e) => {
     key.classList.add('playing');
     audio.currentTime = 0;
     audio.play();
-}
+};
 
 export { removeTransition, playSound };
